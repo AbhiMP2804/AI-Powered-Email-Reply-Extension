70f54a4b91796889d17713363d9a5babb1e1f41c
Abhishek Patwardhan <abhimpat63@gmail.com>
2025-11-18 12:20:05 -0500

    Full Project

---
+++ email-writer-sb/src/main/java/com/email/writer/EmailGeneratorController.java
@@ -0,0 +1,25 @@
+package com.email.writer;
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/email")
+@AllArgsConstructor
+@CrossOrigin(origins = "*")
+public class EmailGeneratorController {
+
+
+    private EmailGeneratorService emailGeneratorService;
+
+    @PostMapping("/generate")
+    public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest)
+    {
+        String response = emailGeneratorService.generateEmailReply(emailRequest);
+        return ResponseEntity.ok(response);
+    }
+
+}
